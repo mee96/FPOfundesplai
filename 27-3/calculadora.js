ede23ee9e55856d6282aeb3a2548224d8ac44ff8
mee96 <carmenmc96@gmail.com>
2026-03-27 08:46:09 +0100

    calculadora

---
+++ 27-3/calculadora.js
@@ -0,0 +1,39 @@
+console.log("hola calculadora");
+
+function obtenerValores() {
+  const n1 = Number(document.getElementById("num1").value);
+  const n2 = Number(document.getElementById("num2").value);
+  return { n1, n2 };
+}
+
+function mostrarResultado(valor) {
+  document.getElementById("resultado").textContent = valor;
+}
+
+
+document.getElementById("sumar").addEventListener("click", () => {
+  const { n1, n2 } = obtenerValores();
+  mostrarResultado(n1 + n2);
+});
+
+document.getElementById("restar").addEventListener("click", () => {
+  const { n1, n2 } = obtenerValores();
+  mostrarResultado(n1 - n2);
+});
+
+document.getElementById("multiplicar").addEventListener("click", () => {
+  const { n1, n2 } = obtenerValores();
+  mostrarResultado(n1 * n2);
+});
+
+document.getElementById("dividir").addEventListener("click", () => {
+  const { n1, n2 } = obtenerValores();
+
+  if (n2 === 0) {
+    mostrarResultado("No se puede dividir entre 0");
+    return;
+  }
+
+  mostrarResultado(n1 / n2);
+});
+

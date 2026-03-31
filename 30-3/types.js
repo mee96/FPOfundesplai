ffa34a753a28bbb02bea167521cd88c937c32b32
mee96 <carmenmc96@gmail.com>
2026-03-31 19:11:58 +0200

    local storage

--- 30-3/types.js
+++ 30-3/types.js
@@ -1,38 +1,10 @@
 'use strict';
 
-const inputname = document.querySelector(".js-name")
-const inputapellido = document.querySelector(".js-apellido")
-const button = document.querySelector(".btn-submit")
-const bdaydate =document.querySelector(".js-bday")
-const saludo = document.querySelector(".js-saludo")
-
-function handleClick (ev){
-    console.log("ha clickado")
-
-    const name = inputname.value
-    const apellido = inputapellido.value
-    const cumple = bdaydate.value
-    console.log(`El nombre es: ${name} ${apellido} y la fecha de nacimiento es: ${cumple}`);
-    saludar(name);   
-    localStorage.setItem("nombre",name)
-}
-
-function saludar (string){
-    saludo.innerHTML = ("Hola " + string)
-}
-
-button.addEventListener("click" , handleClick)
-
-const nombreGuardado = localStorage.getItem("nombre");
-
-if (nombreGuardado) {
-    inputname.value = nombreGuardado;
-     saludar(nombreGuardado)
-}
-
-
-
-// -------objeto---------
+const inputname = document.querySelector(".js-name");
+const inputapellido = document.querySelector(".js-apellido");
+const button = document.querySelector(".btn-submit");
+const bdaydate = document.querySelector(".js-bday");
+const saludo = document.querySelector(".js-saludo");
 
 
 const formData = {
@@ -47,3 +19,36 @@ const formData = {
   checkbox2: false,
   description: ""
 };
+
+function handleClick(ev) {
+  console.log("ha clickado");
+
+  formData.name = inputname.value;
+  formData.lastname = inputapellido.value;
+  formData.birthday = bdaydate.value;
+
+  console.log(`El nombre es: ${formData.name} ${formData.lastname} y la fecha de nacimiento es: ${formData.birthday}`);
+  
+  saludar(formData.name);
+
+  localStorage.setItem("datosFormulario", JSON.stringify(formData));
+}
+
+function saludar(string) {
+  saludo.innerHTML = "Hola " + string;
+}
+
+button.addEventListener("click", handleClick);
+
+const datosGuardados = localStorage.getItem("datosFormulario");
+
+if (datosGuardados) {
+  
+  const datosParseados = JSON.parse(datosGuardados);
+  
+  inputname.value = datosParseados.name;
+  inputapellido.value = datosParseados.lastname;
+  bdaydate.value = datosParseados.birthday;
+  
+  saludar(datosParseados.name);
+}

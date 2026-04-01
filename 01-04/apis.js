22dbea18efd6fbea899f5fd2b639d4360c527e41
mee96 <carmenmc96@gmail.com>
2026-04-01 13:14:05 +0200

    random

--- 01-04/apis.js
+++ 01-04/apis.js
@@ -1,32 +1,31 @@
-
 const button = document.getElementById('loadBtn');
 const container = document.getElementById('productContainer');
 
 button.addEventListener('click', () => {
-  
+ 
   container.innerHTML = ''; 
 
-  
   fetch('https://dummyjson.com/products')
     .then(res => res.json())
     .then(data => {
-      
-      const topThree = data.products.slice(26, 29);
-      console.log('Productes carregats:', topThree);
+      const randomProducts = data.products
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 3);
 
-      topThree.forEach(product => {
-    
+      console.log('Productes aleatoris carregats:', randomProducts);
+
+      randomProducts.forEach(product => {
+        
         const box = document.createElement('div');
         box.className = 'product-box';
+        
         const discountRounded = Math.round(product.discountPercentage);
 
         box.innerHTML = `
           <div class="discount-badge">-${discountRounded}%</div>
-          
           <img src="${product.thumbnail}" alt="${product.title}">
           <h3>${product.title}</h3>
           <p class="price">$${product.price}</p>
-          
         `;
 
         container.appendChild(box);
